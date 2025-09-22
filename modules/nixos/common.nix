4acdcd6d77ada5f29d47df7ff0879dc9bc88cd1e
Hayden Bohlen <bohlenhayden@gmail.com>
2025-09-22 23:20:06 +0000

    Add 1Password to the list of allowed unfree packages

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -28,6 +28,7 @@
 
   # Enable the unfree packages we need.
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "1password"
     "1password-cli"
     "1password-gui"
     "vivaldi"
