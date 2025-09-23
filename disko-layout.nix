5ec7849dc0baee84da4561dbf0e8a2d32b570141
Hayden Bohlen <bohlenhayden@gmail.com>
2025-09-23 19:09:14 +0000

    Refactor disko-layout.nix for disk and partition setup

--- disko-layout.nix
+++ disko-layout.nix
@@ -1,35 +1,87 @@
-# In your disko-layout.nix, update the datasets section:
-datasets = {
-  "local/root" = {
-    type = "zfs_fs";
-    mountpoint = "/";
-    options = {
-      mountpoint = "legacy";
+# /disko-layout.nix
+{ device ? "/dev/disk/by-id/REPLACE_WITH_YOUR_DISK_ID", ... }:
+{
+  disko.devices = {
+    disk = {
+      main = {
+        device = device;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              name = "ESP";
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            swap = {
+              size = "8G";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "cryptroot";
+                settings = {
+                  allowDiscards = true;
+                };
+                content = {
+                  type = "zfs";
+                  pool = "rpool";
+                };
+              };
+            };
+          };
+        };
+      };
     };
-    postCreateHook = ''
-      zfs snapshot rpool/local/root@blank
-    '';
-  };
-  "local/nix" = {
-    type = "zfs_fs";
-    mountpoint = "/nix";
-    options = {
-      mountpoint = "legacy";
-      "com.sun:auto-snapshot" = "false";
+    zpool = {
+      rpool = {
+        type = "zpool";
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
+        rootFsOptions = {
+          compression = "zstd";
+          acltype = "posixacl";
+          xattr = "sa";
+          relatime = "on";
+          mountpoint = "none";
+        };
+        datasets = {
+          "local/root" = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            postCreateHook = ''
+              zfs snapshot rpool/local/root@blank
+            '';
+          };
+          "local/nix" = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+            options."com.sun:auto-snapshot" = "false";
+          };
+          "safe/persist" = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+          };
+          "safe/home" = {
+            type = "zfs_fs";
+            options.mountpoint = "legacy";
+          };
+        };
+      };
     };
   };
-  "safe/persist" = {
-    type = "zfs_fs";
-    mountpoint = "/persist";
-    options = {
-      mountpoint = "legacy";
-    };
-  };
-  "safe/home" = {
-    type = "zfs_fs";
-    mountpoint = "/home";
-    options = {
-      mountpoint = "legacy";
-    };
-  };
-};
+}
