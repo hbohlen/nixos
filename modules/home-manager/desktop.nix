45a112c78baa610cbcc2fdb375c327ca86b35cb4
Hayden Bohlen <bohlenhayden@gmail.com>
2025-09-24 13:33:43 -0400

    Remove deprecated col.shadow configuration in Hyprland settings

--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -80,8 +80,8 @@
         };
         drop_shadow = true;
         shadow_range = 4;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(1a1a1aee)";
+  shadow_render_power = 3;
+  # col.shadow removed (deprecated in recent Hyprland)
       };
 
       # Animations
